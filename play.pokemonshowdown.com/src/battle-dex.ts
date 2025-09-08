916b3e6624eb7431b752e227ddc080fa6e326011
BeeruhBeement <58148912+BeeruhBeement@users.noreply.github.com>
2025-09-08 16:45:11 +0200

    dental to denise

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -989,7 +989,7 @@ class ModdedDex {
 					case 'gen2crystalseviiislands':
 						data.category = Dex.getCSICategory(data.type);	
 						break;
-					case 'gen3dental':
+					case 'gen3denise':
 						data.category = Dex.getGen3ModCategory(data.type);	
 						break;
 					default: 
